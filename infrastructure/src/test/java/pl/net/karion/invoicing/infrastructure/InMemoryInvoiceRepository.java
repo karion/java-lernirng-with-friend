dc6f6edd46996493f220c7d15ed769468b46eeb7
Tomasz Szymczyk <tomasz.szymczyk@karion.net.pl>
2026-02-12 20:12:46 +0000

    fix some files

---
+++ infrastructure/src/test/java/pl/net/karion/invoicing/infrastructure/InMemoryInvoiceRepository.java
@@ -0,0 +1,28 @@
+package pl.net.karion;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import pl.net.karion.application.invoicing.InvoiceRepository;
+import pl.net.karion.domain.invoicing.Invoice;
+import pl.net.karion.domain.invoicing.InvoiceId;
+
+public class InMemoryInvoiceRepository implements InvoiceRepository{
+
+    private final Map<InvoiceId, Invoice> storage = new HashMap<>();
+
+    @Override
+    public Optional<Invoice> findById(InvoiceId id) {
+        return Optional.ofNullable(this.storage.get(id));
+    }
+
+    @Override
+    public void save(Invoice invoice) {
+
+        this.storage.put(
+            invoice.id(),
+            invoice
+        );
+    }
+}
